a9627634ac56aed974a5a98d52db4fe118efbbf7
hardy.zhong <hardy.zhong@robosense.cn>
2025-09-17 20:47:11 +0800

    remove unnecessary sleep delay

--- src/postprocess_node.cpp
+++ src/postprocess_node.cpp
@@ -163,7 +163,7 @@ void PostprocessNode::Core() {
       std::this_thread::sleep_for(std::chrono::milliseconds(1));
       continue;
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    // std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 }
 
